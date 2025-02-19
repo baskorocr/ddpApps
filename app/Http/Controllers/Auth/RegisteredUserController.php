767925a2a9dc49014c5ff7c6c6301c05d70a419d
Mahesa <baskorocr@gmail.com>
2025-02-19 09:06:00 +0700

    can null on whatsapp register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'no' => ['string', 'numeric'],
+
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
